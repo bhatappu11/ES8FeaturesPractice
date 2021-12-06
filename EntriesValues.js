35206d6d7f19ee653e0147b933170956c1b81810
Arpitha Bhat <arpithabht12@gmail.com>
2021-12-06 18:13:10 +0530

    [Arpitha]ADDED: practice code for getting Object entries and values

---
+++ EntriesValues.js
@@ -0,0 +1,13 @@
+const fullName = {
+    firstName: 'James',
+    lastName: 'Alex'
+}
+
+console.log(Object.entries(fullName))
+console.log(Object.values(fullName))
+
+const num = [1,2,3,4,5]
+ console.log(Object.entries(num))
+ console.log(Object.values(num))
+
+ console.log(Object.getOwnPropertyDescriptors(fullName))
